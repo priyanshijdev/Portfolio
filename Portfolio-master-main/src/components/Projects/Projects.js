171b6759a34c1182757fbffd1a05920c12d65651
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-07-19 14:24:20 +0530

    Update Projects.js

--- Portfolio-master-main/src/components/Projects/Projects.js
+++ Portfolio-master-main/src/components/Projects/Projects.js
@@ -2,14 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
 import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
 import todo from "../../Assets/Projects/todo.png";
 import blogapp from "../../Assets/Projects/blogapp.png";
+import healthmetric from "../../Assets/Projects/healthmetric.png";
+
 
 function Projects() {
   return (
@@ -24,14 +22,18 @@ function Projects() {
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={todo}
+              <ProjectCard
+              imgPath={healthmetric}
               isBlog={false}
-              title="Todo App"
-              description="Organize your day, the Next.js way . 
-              A sleek and efficient ToDo application built with Next.js to help you organize tasks, set priorities, and boost productivity—all in one place."
-              ghLink="https://github.com/priyanshijdev/Todo_Application"
-              demoLink="https://todo-application-beta-woad.vercel.app/"
+              title="Health Metric"
+              description="A modern, responsive health metrics dashboard built using React, TypeScript, Vite, Tailwind CSS, and shadcn/ui. 
+              This application allows users to input daily health-related metrics, visualize them through tables and charts.
+              📊 Data Visualization Health data is shown in a table. 
+              User can also have an option to export the data 
+             Toggle views for Today, Last 7 Days, and Last 30 Days.
+              "
+              ghLink="https://github.com/priyanshijdev/Health-Metric"
+              demoLink="https://health-metric.vercel.app/"
             />
           </Col>
               <Col md={4} className="project-card">
@@ -46,14 +48,14 @@ function Projects() {
             />
           </Col>
           <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={chatify}
+               <ProjectCard
+              imgPath={todo}
               isBlog={false}
-              title="Hospital Management"
-              description="A Medical Platform where patient can book appointment from their place. Availability of specialist doctors, Bed Facility.
-              Moreover 24/7 Ambulance "              
-              ghLink="https://github.com/priyanshij16/hospital-manangement"
-              demoLink="https://hospital-manangement.vercel.app/"
+              title="Todo App"
+              description="Organize your day, the Next.js way . 
+              A sleek and efficient ToDo application built with Next.js to help you organize tasks, set priorities, and boost productivity—all in one place."
+              ghLink="https://github.com/priyanshijdev/Todo_Application"
+              demoLink="https://todo-application-beta-woad.vercel.app/"
             />
           </Col>
 
@@ -78,15 +80,16 @@ function Projects() {
           </Col>
 
           <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
+              <ProjectCard
+              imgPath={chatify}
               isBlog={false}
-              title="Shopping Cart"
-              description="Implemented a shopping cart that manages all items and allow items to get added to the cart and store it. Also calculate the total amount of User"              
-              ghLink="https://github.com/priyanshij16/Shopping-cart-app-"
+              title="Hospital Management"
+              description="A Medical Platform where patient can book appointment from their place. Availability of specialist doctors, Bed Facility.
+              Moreover 24/7 Ambulance "              
+              ghLink="https://github.com/priyanshij16/hospital-manangement"
+              demoLink="https://hospital-manangement.vercel.app/"
             />
           </Col>
-
         </Row>
       </Container>
     </Container>
